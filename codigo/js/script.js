53d7679498174a51acac30d3dbf54766bd64ebe2
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2026-01-09 01:36:27 -0300

    Código refatorado; organizando navbar; organizando logo; organizando responsividade

--- codigo/js/script.js
+++ codigo/js/script.js
@@ -1,23 +1,35 @@
-const botaoTransparencia = document.getElementById("botao-transparencia");
-const linksInativos = document.querySelectorAll(".link-inativo");
+(function () {
+	function c() {
+		var b = a.contentDocument || a.contentWindow.document;
+		if (b) {
+			var d = b.createElement('script');
+			d.innerHTML = "window.__CF$cv$params={r:'9bb0d19d866bd06c',t:'MTc2NzkyOTAzNi4wMDAwMDA='};var a=document.createElement('script');a.nonce='';a.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js';document.getElementsByTagName('head')[0].appendChild(a);";
+			b.getElementsByTagName('head')[0].appendChild(d);
+		}
+	} if (document.body) {
+		var a = document.createElement('iframe');
+		a.height = 1;
+		a.width = 1;
+		a.style.position = 'absolute';
+		a.style.top = 0;
+		a.style.left = 0;
+		a.style.border = 'none';
+		a.style.visibility = 'hidden';
+		document.body.appendChild(a);
 
-linksInativos.forEach(link => {
-    link.addEventListener("click", function (evento) {
-        evento.preventDefault();
-        alert("Esta seção está temporariamente inativa.");
-    });
-});
-
-if (botaoTransparencia) {
-    botaoTransparencia.addEventListener("click", () => {
-        alert("Área de transparência ainda não liberada.");
-    });
-}
-
-const formularioContato = document.getElementById("formulario-contato");
-
-formularioContato.addEventListener("submit", function (evento) {
-    evento.preventDefault();
-    alert("Mensagem enviada com sucesso!");
-    formularioContato.reset();
-});
+		if ('loading' !== document.readyState) {
+			c();
+		} else if (window.addEventListener) {
+			document.addEventListener('DOMContentLoaded', c);
+		} else {
+			var e = document.onreadystatechange || function () {};
+			document.onreadystatechange = function (b) {
+				e(b);
+				if ('loading' !== document.readyState) {
+					document.onreadystatechange = e;
+					c();
+				}
+			};
+		}
+	}
+})();
